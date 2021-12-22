ebece18adce7a2dffbe52a5d8d1250705014bcb8
ironzombi <span@ironzombi.com>
2021-12-22 20:42:37 +0800

    Create server.go
    basic server
    handlers import path will need to be updated per instance

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"network_go/ch9/handler/handlers"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	srv := &http.Server{
+		Addr: "127.0.0.1:8081",
+		//Handler: http.TimeoutHandler(handlers.DefaultHandler(), 2*time.Minute, ""),
+		Handler:           http.TimeoutHandler(handlers.DefaultMethodsHandler(), 2*time.Minute, ""),
+		IdleTimeout:       5 * time.Minute,
+		ReadHeaderTimeout: time.Minute,
+	}
+
+	l, err := net.Listen("tcp", srv.Addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	wg.Add(1)
+	go func() {
+
+		err := srv.Serve(l)
+		if err != http.ErrServerClosed {
+			error.Error(err)
+		}
+	}()
+
+	wg.Wait()
+
+}
